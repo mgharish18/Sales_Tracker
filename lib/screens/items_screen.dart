97d5147fd417ea91e509b086869f42dc8a494715
mgharish18 <mgharish18@gmail.com>
2022-01-05 23:35:35 +0530

    without_addItem_function

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -14,12 +14,24 @@ class Items extends StatefulWidget {
 class _ItemsState extends State<Items> {
   late final String acc;
   List<String> items = [];
+  final TextEditingController _controller1 = TextEditingController();
+  final TextEditingController _controller2 = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    _controller1.addListener(() {
+      setState(() {});
+    });
     acc = widget.name;
     items = LocalData().getItem() ?? [];
+  }
+
+  @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    super.dispose();
   }
 
   void onSelected(context, value) {
@@ -31,6 +43,104 @@ class _ItemsState extends State<Items> {
               return Dialog(
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10.0)),
+                child: Container(
+                  padding: const EdgeInsets.all(10.0),
+                  height: 145.0,
+                  width: MediaQuery.of(context).size.width - 10.0,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Add a new Item",
+                        style: GoogleFonts.rajdhani(
+                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 30) / 2,
+                            child: TextField(
+                              keyboardType: TextInputType.name,
+                              decoration: InputDecoration(
+                                prefixIcon: const Icon(Icons.fastfood_rounded),
+                                suffixIcon: _controller1.text.isEmpty
+                                    ? Container(
+                                        width: 0,
+                                      )
+                                    : IconButton(
+                                        onPressed: () => _controller1.clear(),
+                                        icon: const Icon(
+                                          Icons.close,
+                                          color: Colors.red,
+                                        )),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.black, width: 2.0),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.blue, width: 2.0),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                labelText: "Name",
+                              ),
+                              controller: _controller1,
+                            ),
+                          ),
+                          SizedBox(
+                            width: (MediaQuery.of(context).size.width - 30) / 3,
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                prefixIcon:
+                                    const Icon(Icons.price_change_rounded),
+                                suffixIcon: _controller2.text.isEmpty
+                                    ? Container(
+                                        width: 0,
+                                      )
+                                    : IconButton(
+                                        onPressed: () => _controller2.clear(),
+                                        icon: const Icon(
+                                          Icons.close,
+                                          color: Colors.red,
+                                        )),
+                                enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.black, width: 2.0),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                focusedBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.blue, width: 2.0),
+                                    borderRadius: BorderRadius.circular(10.0)),
+                                labelText: "Price",
+                              ),
+                              controller: _controller2,
+                            ),
+                          )
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 10.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          TextButton(
+                              onPressed: null,
+                              child: Text(
+                                "ADD",
+                                style: GoogleFonts.rajdhani(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20.0,
+                                    color: Colors.blue),
+                              )),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
               );
             });
     }

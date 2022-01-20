49b5195aa88a63e8c678171b49979a5e94b7cb11
mgharish18 <mgharish18@gmail.com>
2022-01-20 23:32:55 +0530

    last_commit

--- lib/screens/3_sales_history/table_screen.dart
+++ lib/screens/3_sales_history/table_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+import 'package:syncfusion_flutter_core/theme.dart';
 
 import 'package:sales_records/storage/shared_preferences.dart';
 
@@ -15,6 +17,7 @@ class SalesTable extends StatefulWidget {
 class _SalesTableState extends State<SalesTable> {
   late String acc;
   late Map items;
+  List header = [];
 
   @override
   void initState() {
@@ -23,43 +26,26 @@ class _SalesTableState extends State<SalesTable> {
     items = LocalData().getItem(acc);
   }
 
-  List<DataColumn> getColumns() {
-    List header = ['DATE', 'TIME'];
+  List<GridColumn> getColumns() {
+    header = ['DATE', 'TIME'];
     Map itemMap = LocalData().getItem(acc);
     for (var element in itemMap.keys) {
       header.add(element.toString().toUpperCase());
     }
     return header
-        .map((e) => DataColumn(
-            label: Text(e,
+        .map((e) => GridColumn(
+            columnName: e,
+            label: Center(
+              child: Text(
+                e,
                 style: GoogleFonts.rajdhani(
                     fontSize: 25.0,
                     color: Colors.black,
-                    fontWeight: FontWeight.bold))))
+                    fontWeight: FontWeight.bold),
+                overflow: TextOverflow.ellipsis,
+              ),
+            )))
         .toList();
-  }
-
-  List<DataRow> getRows() {
-    List<DataRow> dataRow = [];
-    Map countMap = LocalData().getSalesLog(acc);
-
-    for (var key in countMap.keys) {
-      DateTime datetime = DateTime.parse(key);
-      List<String> datacell = [];
-      datacell.add(DateFormat('d/M/yy').format(datetime));
-      datacell.add(DateFormat.jm().format(datetime));
-      items = LocalData().getItem(acc);
-      for (var x in countMap[key]) {
-        datacell.add('$x');
-      }
-      while (datacell.length < items.length + 2) {
-        datacell.add('0');
-      }
-      dataRow
-          .add(DataRow(cells: datacell.map((e) => DataCell(Text(e))).toList()));
-    }
-
-    return List.from(dataRow.reversed);
   }
 
   @override
@@ -67,47 +53,102 @@ class _SalesTableState extends State<SalesTable> {
     items = LocalData().getItem(acc);
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text(
-          acc,
-          style:
-              GoogleFonts.rajdhani(fontSize: 25.0, fontWeight: FontWeight.bold),
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: Text(
+            acc,
+            style: GoogleFonts.rajdhani(
+                fontSize: 25.0, fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () => setState(() {}),
+                icon: const Icon(Icons.refresh_rounded))
+          ],
         ),
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: () => setState(() {}),
-              icon: const Icon(Icons.refresh_rounded))
-        ],
-      ),
-      body: items.isEmpty
-          ? Center(
-              child: Text(
-                'Add your Products ',
-                style: GoogleFonts.rajdhani(
-                    fontSize: 25.0,
-                    color: Colors.black,
-                    fontWeight: FontWeight.bold),
-              ),
-            )
-          : SingleChildScrollView(
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(),
-              child: SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                physics: const BouncingScrollPhysics(),
-                child: DataTable(
-                  dividerThickness: 3.0,
-                  columns: getColumns(),
-                  rows: getRows(),
-                  dataTextStyle: GoogleFonts.rajdhani(
-                      fontSize: 15.0,
+        body: items.isEmpty
+            ? Center(
+                child: Text(
+                  'Add your Products ',
+                  style: GoogleFonts.rajdhani(
+                      fontSize: 25.0,
                       color: Colors.black,
                       fontWeight: FontWeight.bold),
                 ),
-              )),
-    );
+              )
+            : SfDataGridTheme(
+                data: SfDataGridThemeData(
+                  gridLineColor: Colors.black54,
+                  gridLineStrokeWidth: 1.5,
+                  frozenPaneElevation: 0.0,
+                  frozenPaneLineColor: Colors.black54,
+                  frozenPaneLineWidth: 1.5,
+                ),
+                child: SfDataGrid(
+                    columns: getColumns(),
+                    source: GridSource(account: acc),
+                    verticalScrollPhysics: const BouncingScrollPhysics(),
+                    horizontalScrollPhysics: const BouncingScrollPhysics(),
+                    allowSorting: true,
+                    frozenColumnsCount: 2,
+                    columnWidthMode: ColumnWidthMode.auto,
+                    allowPullToRefresh: true,
+                    gridLinesVisibility: GridLinesVisibility.none,
+                    headerGridLinesVisibility: GridLinesVisibility.horizontal),
+              ));
   }
+}
+
+class GridSource extends DataGridSource {
+  GridSource({required String account}) {
+    dataGridRowsList = getRows(account);
+  }
+  late List<DataGridRow> dataGridRowsList;
+  @override
+  List<DataGridRow> get rows => dataGridRowsList;
+
+  List<DataGridRow> getRows(String acc) {
+    List<DataGridRow> dataRow = [];
+    Map countMap = LocalData().getSalesLog(acc);
+
+    for (var key in countMap.keys) {
+      DateTime datetime = DateTime.parse(key);
+      List<String> datacell = [];
+      datacell.add(DateFormat('d/M/yy').format(datetime));
+      datacell.add(DateFormat.jm().format(datetime));
+      var items = LocalData().getItem(acc);
+      for (var x in countMap[key]) {
+        datacell.add('$x');
+      }
+      while (datacell.length < items.length + 2) {
+        datacell.add('0');
+      }
+      var header = ['DATE', 'TIME'];
+      Map itemMap = LocalData().getItem(acc);
+      for (var element in itemMap.keys) {
+        header.add(element.toString().toUpperCase());
+      }
+      dataRow.add(DataGridRow(
+          cells: datacell
+              .map((e) => DataGridCell(
+                  columnName: header[datacell.indexOf(e)], value: e))
+              .toList()));
+    }
+
+    return List.from(dataRow.reversed);
+  }
+
+  @override
+  DataGridRowAdapter? buildRow(DataGridRow row) => DataGridRowAdapter(
+          cells: row.getCells().map((e) {
+        return Center(
+            child: Text(
+          e.value,
+          style: GoogleFonts.rajdhani(
+              fontSize: 15.0, color: Colors.black, fontWeight: FontWeight.bold),
+          overflow: TextOverflow.ellipsis,
+        ));
+      }).toList());
 }
